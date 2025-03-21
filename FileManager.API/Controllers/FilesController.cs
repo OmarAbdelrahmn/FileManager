9d080c2f53d45a2111686050b57d1c2fcb4272fe
omar <omarfaroq2003@gmail.com>
2025-03-21 14:37:03 +0200

    Add project files.

---
+++ FileManager.API/Controllers/FilesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.API.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+}
