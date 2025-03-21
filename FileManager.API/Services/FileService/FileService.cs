04db7ed6e54a5d93f7311e9af29549a2461ad6d5
omar <omarfaroq2003@gmail.com>
2025-03-21 15:36:50 +0200

    add upload file service

--- FileManager.API/Services/FileService/FileService.cs
+++ FileManager.API/Services/FileService/FileService.cs
@@ -1,20 +1,36 @@
 ﻿using FileManager.API.Contracts.Files;
 using FileManager.API.Entities;
+using FileManager.API.Persistence;
 
 namespace FileManager.API.Services.FileService;
 
-public class FileService : IFileService
+public class FileService(IWebHostEnvironment webHostEnvironment , ApplicationDbcontext dbcontext) : IFileService
 {
-    public Task<Guid> Upload(UpdoadFilesRequest request)
+    private readonly string filepath = $"{webHostEnvironment.WebRootPath}/Uploads";
+    private readonly ApplicationDbcontext dbcontext = dbcontext;
+
+    public async Task<Guid> Upload(UpdoadFilesRequest request)
     {
-        var rondomefilename = Path.GetRandomFileName();
+        var randomefilename = Path.GetRandomFileName();
 
         var uploadedfile = new UploadedFile
         {
             FileName = request.File.FileName,
             ContentType = request.File.ContentType,
-            StoredFileName = rondomefilename,
+            StoredFileName = randomefilename,
             FileExtenstions = Path.GetExtension(request.File.FileName)
         };
+
+        var path = Path.Combine(filepath, randomefilename);
+
+        using var stream =File.Create(path);
+
+        await request.File.CopyToAsync(stream);
+
+
+        await dbcontext.AddAsync(uploadedfile);
+        await dbcontext.SaveChangesAsync();
+
+        return uploadedfile.Id;
     }
 }
