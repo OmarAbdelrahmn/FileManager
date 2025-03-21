0c7fcfb819220165fbf333d1ed50aebe812ae24e
omar <omarfaroq2003@gmail.com>
2025-03-21 14:43:11 +0200

    add dbcontext

---
+++ FileManager.API/Persistence/ApplicationDbcontext.cs
@@ -0,0 +1,9 @@
+﻿using FileManager.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileManager.API.Persistence;
+
+public class ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : DbContext(options)
+{
+    public DbSet<UploadedFile> Files { get; set; } = default!;
+}
