973a21ddac400363f2b6ab8f4bdd22a9250bb312
omar <omarfaroq2003@gmail.com>
2025-03-21 14:54:46 +0200

    add dbcontext configration

--- FileManager.API/Persistence/ApplicationDbcontext.cs
+++ FileManager.API/Persistence/ApplicationDbcontext.cs
@@ -1,9 +1,17 @@
 ﻿using FileManager.API.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace FileManager.API.Persistence;
 
 public class ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : DbContext(options)
 {
     public DbSet<UploadedFile> Files { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
