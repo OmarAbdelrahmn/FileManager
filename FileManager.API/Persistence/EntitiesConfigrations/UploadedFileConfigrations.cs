2d0dc8ead33b9717d1c92d69adea1434bad90d77
omar <omarfaroq2003@gmail.com>
2025-03-21 14:48:47 +0200

    add configration for the file

---
+++ FileManager.API/Persistence/EntitiesConfigrations/UploadedFileConfigrations.cs
@@ -0,0 +1,29 @@
+﻿using FileManager.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FileManager.API.Persistence.EntitiesConfigrations;
+
+public class UploadedFileConfigrations : IEntityTypeConfiguration<UploadedFile>
+{
+    public void Configure(EntityTypeBuilder<UploadedFile> builder)
+    {
+        builder.Property(c => c.FileName)
+            .HasMaxLength(250)
+            .IsRequired();
+        
+        builder.Property(c => c.ContentType)
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        builder.Property(c => c.StoredFileName)
+            .HasMaxLength(250)
+            .IsRequired();
+        
+        builder.Property(c => c.FileExtenstions)
+            .HasMaxLength(10)
+            .IsRequired();
+        
+
+    }
+}
