dabd70528c296b2674d7359a7a917d86958e34a3
omar <omarfaroq2003@gmail.com>
2025-03-22 13:17:27 +0200

    add file setting

---
+++ FileManager.API/Contracts/Files/UpdoadFilesRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace FileManager.API.Contracts.Files;
+
+public class UpdoadFilesRequestValidator : AbstractValidator<UpdoadFilesRequest>
+{
+    public UpdoadFilesRequestValidator()
+    {
+        RuleFor()
+    }
+}
---
+++ FileManager.API/Settings/FileSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace FileManager.API.Settings;
+
+public static class FileSettings
+{
+    public const int MaxFileSizeInMB = 3;
+    public const int MaxFileSizeInBytes = MaxFileSizeInMB * 1024 * 1024;
+}
