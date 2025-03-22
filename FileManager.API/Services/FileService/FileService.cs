51cda15e8163b55a53ea3b586effe2cd7df9ef71
omar <omarfaroq2003@gmail.com>
2025-03-22 12:16:05 +0200

    add upload many files service

--- FileManager.API/Services/FileService/FileService.cs
+++ FileManager.API/Services/FileService/FileService.cs
@@ -1,4 +1,5 @@
-﻿using FileManager.API.Contracts.Files;
+﻿using Azure.Core;
+using FileManager.API.Contracts.Files;
 using FileManager.API.Entities;
 using FileManager.API.Persistence;
 
@@ -11,26 +12,49 @@ public class FileService(IWebHostEnvironment webHostEnvironment , ApplicationDbc
 
     public async Task<Guid> Upload(UpdoadFilesRequest request)
     {
-        var randomefilename = Path.GetRandomFileName();
-
-        var uploadedfile = new UploadedFile
-        {
-            FileName = request.File.FileName,
-            ContentType = request.File.ContentType,
-            StoredFileName = randomefilename,
-            FileExtenstions = Path.GetExtension(request.File.FileName)
-        };
-
-        var path = Path.Combine(filepath, randomefilename);
-
-        using var stream =File.Create(path);
-
-        await request.File.CopyToAsync(stream);
+       var uploadedfile = await SaveFile(request.File);
 
 
         await dbcontext.AddAsync(uploadedfile);
         await dbcontext.SaveChangesAsync();
 
         return uploadedfile.Id;
+    }
+
+    public async Task<IEnumerable<Guid>> UploadManyFiles(UploadManyFilesRequest request)
+    {
+        List<UploadedFile> files = [];
+
+        foreach (var file in request.Files) {
+
+            var uploadedfile = await SaveFile(file);
+            files.Add(uploadedfile);
+        }
+
+        await dbcontext.AddRangeAsync(files);
+        await dbcontext.SaveChangesAsync();
+
+        return files.Select(f => f.Id).ToList();
+    }
+
+    private async Task<UploadedFile> SaveFile (IFormFile file)
+    {
+        var randomefilename = Path.GetRandomFileName();
+
+        var uploadedfile = new UploadedFile
+        {
+            FileName = file.FileName,
+            ContentType = file.ContentType,
+            StoredFileName = randomefilename,
+            FileExtenstions = Path.GetExtension(file.FileName)
+        };
+
+        var path = Path.Combine(filepath, randomefilename);
+
+        using var stream = File.Create(path);
+
+        await file.CopyToAsync(stream);
+
+        return uploadedfile;
     }
 }
