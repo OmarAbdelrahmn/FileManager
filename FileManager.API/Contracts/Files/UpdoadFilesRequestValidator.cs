76c8ee669e9b9bc7624bed13e4f051644a852869
omar <omarfaroq2003@gmail.com>
2025-03-22 17:40:23 +0200

    add file content validation

--- FileManager.API/Contracts/Files/UpdoadFilesRequestValidator.cs
+++ FileManager.API/Contracts/Files/UpdoadFilesRequestValidator.cs
@@ -11,5 +11,25 @@ public class UpdoadFilesRequestValidator : AbstractValidator<UpdoadFilesRequest>
             .Must((request , context) => request.File.Length <= FileSettings.MaxFileSizeInBytes)
             .WithMessage($"Max file size is {FileSettings.MaxFileSizeInMB} MB")
             .When(c=>c.File is not null);
+
+        RuleFor(c => c.File)
+            .Must((request, context) =>
+            {
+                BinaryReader reader = new (request.File.OpenReadStream());
+
+                var headerBytes = reader.ReadBytes(2);
+
+                var fileSequenceHex = BitConverter.ToString(headerBytes);
+
+                foreach (var signature in FileSettings.BlockedSigntures)
+                    if (fileSequenceHex.Equals(fileSequenceHex , StringComparison.OrdinalIgnoreCase))                   
+                        return false;
+                  
+                return true;
+
+            })
+            .WithMessage("Invalid file format")
+            .When(c => c.File is not null);
+        
     }
 }
--- FileManager.API/Settings/FileSettings.cs
+++ FileManager.API/Settings/FileSettings.cs
@@ -4,4 +4,5 @@ public static class FileSettings
 {
     public const int MaxFileSizeInMB = 3;
     public const int MaxFileSizeInBytes = MaxFileSizeInMB * 1024 * 1024;
+    public static readonly string[] BlockedSigntures = ["4D-5A","2F-2A","D0-CF"];
 }
