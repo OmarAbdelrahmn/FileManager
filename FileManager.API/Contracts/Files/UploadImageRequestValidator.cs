7481e882909c50cee26e92f12546d7c553b44e53
omar <omarfaroq2003@gmail.com>
2025-03-24 21:52:00 +0200

    add image validator

---
+++ FileManager.API/Contracts/Files/UploadImageRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FileManager.API.Settings;
+using FluentValidation;
+
+namespace FileManager.API.Contracts.Files;
+
+public class UploadImageRequestValidator : AbstractValidator<UpdoadImagessRequest>
+{
+    public UploadImageRequestValidator()
+    {
+        RuleFor(c => c.Image)
+            .Must((request, context) =>
+            {
+                var extension = Path.GetExtension(request.Image.FileName.ToLower());
+                
+                return FileSettings.AllowedImagesExtensions.Contains(extension);
+            })
+            .WithMessage("file extenion is not allowed ")
+            .When(c => c is not null);
+    }
+}
